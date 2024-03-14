d1cb0376e61f72e8399e89d868dd48d0900536f8
mounir <m.abes@bneder.dz>
2024-03-14 09:33:37 +0100

    Initial commit

---
+++ js/midnight.js
@@ -0,0 +1,27 @@
+// Check if the user has a preferred color scheme stored
+const darkModePreferred = localStorage.getItem('darkMode');
+
+// If dark mode is preferred, apply it to the body
+if (darkModePreferred === 'true') {
+  document.body.classList.add('dark-mode');
+}
+document.addEventListener('abort', function(){
+    
+})
+// Toggle dark mode
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeButton = document.querySelector('.midnight-btn');
+    
+    if (darkModeButton) {
+      darkModeButton.addEventListener('click', function(event) {
+        // Check if the clicked element is the dark mode button
+        if (event.target === darkModeButton) {
+          document.body.classList.toggle('dark-mode');
+
+          // Save the user's preference to local storage
+          const isDarkMode = document.body.classList.contains('dark-mode');
+          localStorage.setItem('darkMode', isDarkMode);
+        }
+      });
+    }
+});
